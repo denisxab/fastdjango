6b1a12a9bab5a0dc5a8772172f09584bc79d2ba0
Your Name <you@example.com>
2023-11-03 15:26:42 +0300

    "Пт 03 ноя 2023 15:26:42" -

--- fhelp/fixtures.py
+++ fhelp/fixtures.py
@@ -12,13 +12,11 @@ def loaddata_(file: Path):
     json_data = json.loads(file.read_text(encoding="utf-8"))
 
     res = 0
-    for row in json_data["rows"]:
+    for row in json_data["data"]:
         sql_query = "INSERT INTO {name} ({keys}) VALUES ({values});".format(
             name=json_data["model"],
-            keys=", ".join(row.keys()),
-            values=", ".join(
-                [f"'{v}'" if isinstance(v, str) else str(v) for v in row.values()]
-            ),
+            keys=", ".join(json_data["column_name"]),
+            values=", ".join([f"'{v}'" if isinstance(v, str) else str(v) for v in row]),
         )
         res += sql_write(sql_query)
     return res
@@ -29,6 +27,12 @@ def dumpdata_(name_table: str):
     sql_query = f"SELECT * FROM {name_table}"
     res = sql_read(sql_query)
     json_data = json.dumps(
-        {"model": f"{name_table}", "rows": res}, ensure_ascii=False, indent=4
+        {
+            "model": f"{name_table}",
+            "column_name": list(res[0].keys()),
+            "data": [list(r.values()) for r in res],
+        },
+        ensure_ascii=False,
+        indent=2,
     )
     return json_data
