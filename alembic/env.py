0dd0eb8579b69842b6044e370804f023ecd05571
Your Name <you@example.com>
2023-11-03 15:12:45 +0300

    "Пт 03 ноя 2023 15:12:45" -

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,5 @@
-
 from alembic import context
-from fhelp.alembic_env import run_migrations_offline,run_migrations_online
-
+from fhelp.alembic_env import run_migrations_offline, run_migrations_online
 
 # Если Alembic запущен через 'stamped'
 if context.is_offline_mode():
